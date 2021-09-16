7f08247defe7bb74b90852ca84ee07717dea3e66
enginbyram <enginbayram.eru@gmail.com>
2021-09-16 13:53:30 +0300

    sidebar is now a pop-up, there's no close button,

--- resources/sidebar.js
+++ resources/sidebar.js
@@ -1,11 +1,18 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("left-sidebar").style.width = "250px";
-    // document.getElementById("main").style.marginLeft = "250px";
-  }
-
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("left-sidebar").style.width = "0";
-    // document.getElementById("main").style.marginLeft = "0";
-  } 
+function toggleNav() {
+    if (document.getElementById("left-sidebar").style.width == "250px") {
+      return close();
+    }
+    return open();
+}
+// Set the width of the sidebar to 250px and draw border
+function open() {
+  document.getElementById("left-sidebar").style.width = "250px";
+  document.getElementById("left-sidebar").style.opacity = "1";
+  document.getElementById("left-sidebar").style.borderColor = "#c4c4c4";
+}
+// Set the width of the sidebar to 250px and unset border
+function close() {
+  document.getElementById("left-sidebar").style.width = "0px";
+  document.getElementById("left-sidebar").style.opacity = "0";
+  document.getElementById("left-sidebar").style.borderColor = "transparent";
+}
